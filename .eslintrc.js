dd96982ac51930b09c1348003a394c209be1f4ec
Chien HD <chien.hoang@savvycomsoftware.com>
2020-10-06 09:42:52 +0700

    eslint and setting format with is prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,6 @@ module.exports = {
   ],
   plugins: ['prettier'],
   // add your custom rules here
-  // add your custom rules here
   rules: {
     'no-console': 0,
     'prettier/prettier': ['error', { semi: false }],
